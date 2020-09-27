e1d75fc2e9bd418e45c61ff20151b1dfe0072d7b
Mradul Jain <er.mradulrathore@gmail.com>
2020-09-27 17:12:05 +0530

    todo app files

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/todolist.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+ /*
+    List<Todo> todos = List<Todo>();
+    DbHelper helper = DbHelper();
+    helper.initializeDb().then(
+        (result) => helper.getTodos().then(
+          (result) => todos=result)
+    );
+    DateTime today = DateTime.now();
+    Todo todo = Todo("Buy Oranges",2,today.toString(),"And make sure they are good");
+    var result = helper.insertTodo(todo);
+  */
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todos',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Todos'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+   MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: TodoList(),
+    );
+  }
+}
